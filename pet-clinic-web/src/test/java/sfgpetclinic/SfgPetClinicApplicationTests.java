fa10fa6930e8f86bc2e52e053d447cd38e3dabce
amal2004 <amal2004@gmail.com>
2018-08-26 17:59:17 -0400

    Refactoring to multi module maven project. Closes #1.

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package amal.springframework.sfgpetclinic;
+package sfgpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- src/main/java/amal/springframework/sfgpetclinic/model/Vet.java
+++
@@ -1,4 +0,0 @@
-package amal.springframework.sfgpetclinic.model;
-
-public class Vet extends Person {
-}

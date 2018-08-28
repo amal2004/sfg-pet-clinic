1f7ef5485b0bf502fb183e2e3c537740a7438c51
amal2004 <amal2004@gmail.com>
2018-08-27 21:30:40 -0400

    Implement interfaces for for services. Closes #5

---
+++ pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package amal.springframework.sfgpetclinic.services;
+
+import amal.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package amal.springframework.sfgpetclinic.services;
+
+import amal.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

59f61c5b77c25a7895b5f12cf3a336ab64048a9c
amal2004 <amal2004@gmail.com>
2018-08-28 21:35:09 -0400

    Refactor Service Interfaces to Common Base Interface. Closes #11

--- pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package amal.springframework.sfgpetclinic.services;
 
 import amal.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }

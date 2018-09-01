27fce2038ef26721bcc8c7fa39cdc99d8625ae1a
amal2004 <amal2004@gmail.com>
2018-08-31 20:47:53 -0400

    Listing vets on index page. Closes #15

--- pet-clinic-web/src/main/java/amal/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/amal/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,33 @@
 package amal.springframework.sfgpetclinic.controllers;
 
+import amal.springframework.sfgpetclinic.model.Owner;
+import amal.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+
+
+    private final OwnerService ownerService;
+
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index","/index.html"})
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
+
+        Set<Owner> set = ownerService.findAll();
+        set.forEach(System.out::println);
         return "owners/index";
     }
 }

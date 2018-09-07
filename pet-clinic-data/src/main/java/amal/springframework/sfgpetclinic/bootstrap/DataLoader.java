7186607e07ac3a8dbff31aa52f0dbe166e91ddf7
amal2004 <amal2004@gmail.com>
2018-09-07 13:23:42 -0400

    Add PetTypes in with Bootsrap(startup). Closes #33

--- pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package amal.springframework.sfgpetclinic.bootstrap;
 
 import amal.springframework.sfgpetclinic.model.Owner;
+import amal.springframework.sfgpetclinic.model.PetType;
 import amal.springframework.sfgpetclinic.model.Vet;
 import amal.springframework.sfgpetclinic.services.OwnerService;
+import amal.springframework.sfgpetclinic.services.PetTypeService;
 import amal.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setId(1L);

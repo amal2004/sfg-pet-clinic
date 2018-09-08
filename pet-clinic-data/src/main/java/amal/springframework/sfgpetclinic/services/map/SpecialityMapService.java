298b79830dae01b7e027266924f07302936b3f0e
amal2004 <amal2004@gmail.com>
2018-09-07 23:11:59 -0400

    Create Specialty Map Service Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package amal.springframework.sfgpetclinic.services.map;
+
+import amal.springframework.sfgpetclinic.model.Speciality;
+import amal.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findId(aLong);
+    }
+}

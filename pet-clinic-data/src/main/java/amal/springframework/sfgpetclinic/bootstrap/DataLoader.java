76a3c576d4fb9df65d78a297b9819086778eebdd
amal2004 <amal2004@gmail.com>
2018-09-08 00:06:46 -0400

    Create Specialties add to vets on startup (Bootsrap). Closes #35

--- pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package amal.springframework.sfgpetclinic.bootstrap;
 
-import amal.springframework.sfgpetclinic.model.Owner;
-import amal.springframework.sfgpetclinic.model.Pet;
-import amal.springframework.sfgpetclinic.model.PetType;
-import amal.springframework.sfgpetclinic.model.Vet;
+import amal.springframework.sfgpetclinic.model.*;
 import amal.springframework.sfgpetclinic.services.OwnerService;
 import amal.springframework.sfgpetclinic.services.PetTypeService;
+import amal.springframework.sfgpetclinic.services.SpecialtyService;
 import amal.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,22 +16,46 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private SpecialtyService specialtyService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-       PetType dog = new PetType();
+
+        int count = petTypeService.findAll().size();
+
+        if (count == 0 ){
+            loadData();
+        }
+
+    }
+
+    private void loadData() {
+        PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
+
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialtyService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialtyService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("dentistry");
+        Speciality savedDentistry = specialtyService.save(dentistry);
 
 
         Owner owner1 = new Owner();
@@ -74,16 +96,16 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(savedRadiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-
+        vet2.getSpecialities().add(savedSurgery);
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
-
     }
 }
--- pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/amal/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package amal.springframework.sfgpetclinic.services.map;
 
+import amal.springframework.sfgpetclinic.model.Speciality;
 import amal.springframework.sfgpetclinic.model.Vet;
+import amal.springframework.sfgpetclinic.services.SpecialtyService;
 import amal.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,11 @@ import java.util.Set;
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService{
 
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -22,6 +29,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if(object.getSpecialities().size() > 0){
+            object.getSpecialities().forEach(specility -> {
+                if(specility.getId() == null){
+                    Speciality savedSpecialty = specialtyService.save(specility);
+                    specility.setId(savedSpecialty.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
 
